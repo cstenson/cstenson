5e098639fef2fb252404b752be48ddb0c061b391
Dee Wu <deewu@dee-wus-mac-pro.hsc.net.ou.edu>
2012-08-23 16:19:46 -0500

    add

--- helloworld/src/hello.java
+++ helloworld/src/hello.java
@@ -7,6 +7,8 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		hello2 h1 = new hello2();
+		System.out.println(h1.printIt());
 	}
 
 }
